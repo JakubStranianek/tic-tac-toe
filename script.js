ce962a752d47c93b458e31db2b4df63d87871877
JakubStranianek <j.stranianek@gmail.com>
2022-01-30 23:57:04 +0100

    adding icons and changing players with hover

--- script.js
+++ script.js
@@ -11,4 +11,39 @@ function checkFocus() {
     }
 }
 
+$(".grid-item").on("click", function(){
+    //ak je na tahu x
+    if ($("#whoGo").attr("src") == "assets/icon-x-outline.svg") {
+        if ($(this).find("img").length > 0){
+            alert("Policko je obsadene");
+        } else {
+            $(this).prepend('<img src="assets/icon-x.svg" id="x" alt="icon-x">');
+            $("#whoGo").attr("src", "assets/icon-o.svg")
+        }
+    } else {
+        //ak je na tahu o
+        if ($(this).find("img").length > 0){
+            alert("Policko je obsadene");
+        } else {
+            $(this).prepend('<img src="assets/icon-o.svg" id="o" alt="icon-o">');
+            $("#whoGo").attr("src", "assets/icon-x-outline.svg");
+        }
+    }
+})
+
+$(".grid-item").mouseenter(function () { 
+    if ($("#whoGo").attr("src") == "assets/icon-x-outline.svg") {
+        $(this).css('background-image', 'url(' + "assets/icon-x-outline-blue.svg" + ')').css('background-repeat', 'no-repeat').css('background-position', '50% 50%');
+    } else {
+        $(this).css('background-image', 'url(' + "assets/icon-o-outline-yellow.svg" + ')').css('background-repeat', 'no-repeat').css('background-position', '50% 50%');
+    } 
+});
+
+$(".grid-item").mouseleave(function () { 
+        $(this).css({background: "#1F3641"});
+});
+
+function skontrolujVysledok() {
+     
+}
 
